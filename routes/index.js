853cbaf4248976fb181fac93eb1e66d90340361d
Alex Beam <alexbeam@umich.edu>
2015-09-02 15:41:40 -0400

    pretty dates

--- routes/index.js
+++ routes/index.js
@@ -42,14 +42,18 @@ router.post('/addposting', function(req, res) {
     var collection = db.get('postingcollection');
 
     var expdate = new Date(new Date().setDate(new Date().getDate() + 7))
+    var created = new Date()
+    var created_format = (created.getMonth() + 1) + '/' + created.getDate() + '/' + (created.getYear()-100+2000)
+    var exp_format = (expdate.getMonth() + 1) + '/' + expdate.getDate() + '/' + (expdate.getYear()-100+2000)
 
     // Submit to the DB
     collection.insert({
         "title" : Title,
         "club" : Club,
         "email" : Email,
-        "created": new Date(),
-        "expireAt": expdate
+        "created": created_format,
+        "expireAt": expdate,
+        "expires" : exp_format
     }, function (err, doc) {
         if (err) {
             // If it failed, return error
