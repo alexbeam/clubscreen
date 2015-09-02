3c5e47e912790507fdb1c0b6a30c6b841ef952f8
aemaly <aemaly@umich.edu>
2015-09-02 17:35:51 -0400

    this is a commit fora pending pull

--- routes/index.js
+++ routes/index.js
@@ -82,7 +82,7 @@ router.post('/addposting', function(req, res) {
         else {
             //send email
             var mailOptions={
-                to : "antoninamalyarenko@gmail.com",
+                to : postEmail,
                 subject : "New Post from ClubScreenWolverine- Please Read",
                 text: "Title: " + postTitle + " Club/Organization: " + postClub + " Email: " + postEmail
                 + " Involvement: " + postInvolvement + " Position Type: " + positionType + " Club Type: "
