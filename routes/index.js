82931016bb69c617abbba25651adca5bbd9a5a00
Alex Beam <alexbeam@umich.edu>
2015-09-03 17:05:00 -0400

    fixed timing and made some partials. also cleared db

--- routes/index.js
+++ routes/index.js
@@ -52,7 +52,6 @@ router.post('/newapplicant', function(req,res){
     var postPhone = req.body.phone;
     var postYear = req.body.year;
     var postID = req.body.id.toString();
-
     var collection = db.get('postingcollection');
 
     collection.findOne({_id: postID}, function(err, posting) {
@@ -63,7 +62,7 @@ router.post('/newapplicant', function(req,res){
         var mailOptions={
             to : mailList,
             subject : "New Post from ClubScreenWolverine- Please Read",
-            text: "Name: " + postFirstName + " " + postLastName + " You applied for the "+posting.title
+            text: "Name: " + postFirstName + " " + postLastName + " You applied for the "+ posting.title
         };
         console.log(mailOptions);
         transporter.sendMail(mailOptions, function(error, response){
@@ -72,13 +71,11 @@ router.post('/newapplicant', function(req,res){
                 res.end("error");
             }else{
                 console.log("Message sent: " + response.message);
-                res.end("sent");
                 // And forward to success page
                 res.redirect("postinglist");
             }
         });
     });
-
 });
 
 /*--------Post Submission Data-----------------------------------------*/
@@ -109,14 +106,14 @@ router.post('/addposting', function(req, res) {
     var positionType = req.body.position_type;
     var clubType = req.body.club_type;
     var clubDescr = req.body.description;
-    var daysOnSite = req.body.days;
+    var daysOnSite = Number(req.body.days);
 
     // Set our collection
     var collection = db.get('postingcollection');
 
     //Timing
-    var expdate = new Date(new Date().setDate(new Date().getDate() + daysOnSite));
     var created = new Date();
+    var expdate = new Date(new Date().setDate(new Date().getDate() + daysOnSite));
     var created_format = (created.getMonth() + 1) + '/' + created.getDate() + '/' + (created.getYear()-100+2000);
     var exp_format = (expdate.getMonth() + 1) + '/' + expdate.getDate() + '/' + (expdate.getYear()-100+2000);
 
@@ -131,7 +128,8 @@ router.post('/addposting', function(req, res) {
         "description" : clubDescr,
         "created": created_format,
         "expireAt": expdate,
-        "expires" : exp_format
+        "expires" : exp_format,
+        "received" : 0
     }, function (err, doc) {
         if (err) {
             // If it failed, return error
