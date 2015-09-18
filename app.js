f8af40618ce180d00b388ff8d0471cdaf04d8d6d
Alex Beam <alexbeam@umich.edu>
2015-09-18 16:18:27 -0400

    db stuff

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var routes = require('./routes/index');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
