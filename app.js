fdab1d8c23f283e2132a6abb630f94cdd0d8735c
Alex Beam <alexbeam@umich.edu>
2015-09-18 16:23:54 -0400

    db stuff

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ var routes = require('./routes/index');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
